df9ea7d26741eb3f8118a1c677f78632d9144d03
Zian Jiang <zj444@nyu.edu>
2021-11-03 20:10:50 -0400

    Update get_recommendations.py

--- get_recommendations.py
+++ get_recommendations.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     model.load_state_dict("model.pt")
     
     # give a list of indices to check recommendations for 
-    list_to_check = [0, 6, 10]
+    indices_to_check = [0, 6, 10]
     
     z = model.encode(test_data.x, test_data.edge_index)
     prob_adj = z @ z.t()
@@ -20,5 +20,5 @@ if __name__ == "__main__":
     # fill diagonal with 0's so that we don't recommend the orginal paper itself
     prob_adj = prob_adj.fill_diagonal_(0.0)
     prob_adj = prob_adj.detach().cpu()
-    sorted, indices = torch.sort(prob_adj[l], descending=True)
+    sorted, indices = torch.sort(prob_adj[indices_to_check], descending=True)
     recs = indices[:, :10]
