260e76cef78528667b45db6b1afc1ff515bbaedf
Zian Jiang <zj444@nyu.edu>
2021-11-03 20:19:27 -0400

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -7,10 +7,10 @@ from torch_geometric.datasets import FacebookPagePage
   
 @torch.no_grad()
 def test(data):
-    model.eval()
-    z = model.encode(data.x, data.edge_index)
-    out = model.decode(z, data.edge_label_index).view(-1).sigmoid()
-    return roc_auc_score(data.edge_label.cpu().numpy(), out.cpu().numpy())
+	model.eval()
+	z = model.encode(data.x, data.edge_index)
+	out = model.decode(z, data.edge_label_index).view(-1).sigmoid()
+	return roc_auc_score(data.edge_label.cpu().numpy(), out.cpu().numpy())
   
 def prepare_data():
 	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -19,15 +19,13 @@ def prepare_data():
     T.RandomLinkSplit(num_val=0.05, num_test=0.1, is_undirected=True,
                       add_negative_train_samples=False),
 	])
-
 	dataset = FacebookPagePage("../data", transform=transform)
 	train_data, val_data, test_data = dataset[0]
-	
 	return train_data.to(device), val_data.to(device), test_data.to(device)
    
 def get_recommendations(data, indices, n=10):
 	z = model.encode(data.x, data.edge_index)
-    prob_adj = z @ z.t()
+	prob_adj = z @ z.t()
     
     # fill diagonal with 0's so that we don't recommend the orginal paper itself
     prob_adj = prob_adj.fill_diagonal_(0.0)
